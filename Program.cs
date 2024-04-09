73a13952d169ceb722d849424852a02f69741e60
Dmitry3Lyalyuk <165943906+Dmitry3Lyalyuk@users.noreply.github.com>
2024-04-09 23:39:47 +0300

    add: data type

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿bool isValid = false;
+sbyte level = 11;
+short a1 =32000;
+int score =-12312313;
+var range =-9233132123L;
+byte age =101;
+ushort value =60100;
+ulong range2=1200000L;
+float number =-33.31F;
+char ch1 = '\u0041';
+string name = "Dmitry";
+
+Type myType = typeof(bool);
+Console.WriteLine("Тип: " + myType);
+Console.WriteLine("Пример: " + !isValid);
+Console.WriteLine ("Size: " + sizeof(bool));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(sbyte));
+Console.WriteLine("Пример: " + level);
+Console.WriteLine ("Size: " + sizeof(sbyte));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(short));
+Console.WriteLine("Пример: " + a1);
+Console.WriteLine ("Size: " + sizeof(short));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(int));
+Console.WriteLine("Пример: " + score);
+Console.WriteLine ("Size: " + sizeof(int));
+
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(long));
+Console.WriteLine("Пример: " + range);
+Console.WriteLine ("Size: " + sizeof(long));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(byte));
+Console.WriteLine("Пример: " + age);
+Console.WriteLine ("Size: " + sizeof(byte));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(ushort));
+Console.WriteLine("Пример: " + value);
+Console.WriteLine ("Size: " + sizeof(ushort));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(ulong));
+Console.WriteLine("Пример: " + range2);
+Console.WriteLine ("Size: " + sizeof(ulong));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(float));
+Console.WriteLine("Пример: " + number);
+Console.WriteLine ("Size: " + sizeof(float));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(char));
+Console.WriteLine("Пример: " + ch1);
+Console.WriteLine ("Size: " + sizeof(char));
+
+Console.WriteLine();
+Console.WriteLine("Тип: " + typeof(string));
+Console.WriteLine("Пример: " + name);
+
+
+
+
+
+
+
+
+
